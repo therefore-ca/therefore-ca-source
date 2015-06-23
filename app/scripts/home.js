b6b1f18f0b1dda1fea724f1bb4766923a8f5008f
Sean <sean@therefore.ca>
2015-06-23 16:05:48 -0400

    meta

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -1,18 +1,6 @@
 $( document ).ready(function() {
 	'use strict';
 
-	$('a.menu-open').click(function(e) {
-		e.preventDefault();
-		$('a.menu-open').addClass('mobile-hidden');	
-		$('.menu-container').removeClass('mobile-hidden closed').addClass('open');	
-	});
-
-	$('a.menu-close').click(function(e) {
-		e.preventDefault();
-		$('.menu-container').addClass('mobile-hidden open').addClass('closed');
-		$('a.menu-open').removeClass('mobile-hidden');	
-	});
-
 	var carouselController = function(id) {
 		var items = $(id + ' ul.slides li'),
 		controls = $(id + ' ul.controls li'),
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
 $.material.init();
+
+$( document ).ready(function() {
+	'use strict';
+
+	$('a.menu-open').click(function(e) {
+		e.preventDefault();
+		$('a.menu-open').addClass('mobile-hidden');	
+		$('.menu-container').removeClass('mobile-hidden closed').addClass('open');	
+	});
+
+	$('a.menu-close').click(function(e) {
+		e.preventDefault();
+		$('.menu-container').addClass('mobile-hidden open').addClass('closed');
+		$('a.menu-open').removeClass('mobile-hidden');	
+	});
+
+});
