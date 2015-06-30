98f6ebd6dfecbdeb7ce6ac8e32449e6b00d3a9a4
Hurel Clément <clement.hurel@gmail.com>
2015-06-30 12:44:52 -0400

    Remove mandrill key from public javascript and put it into a middleware.

--- app/scripts/contact.js
+++ app/scripts/contact.js
@@ -7,55 +7,26 @@ $( document ).ready(function() {
   {
     preventSubmit: true,
     submitError: function($form, event, errors) {
-                // Here I do nothing, but you could do something like display 
-                // the error messages to the user, log, etc.
-              },
-              submitSuccess: function($form, event) {
-                event.preventDefault();
-                var name = $('#contact-form input#name').val();
-                var email = $('#contact-form input#email').val();
-                var comment = $('#contact-form #comment').val();
-                $.ajax({
-                  type: 'POST',
-                  url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-                        //url:'/contact-process',
-                        data: {
-                          'key': 'yU1xPprQ1LjIdmsbvqxCaQ',
-                              //'name' : name,
-                              //'email' : email,
-                              //'comment': comment
-                              'message': {
-                                'from_email': 'contact@therefore.ca',
-                                'to': [
-                                //{
-                                //  'email': 'clement@therefore.ca',
-                                //  'name': 'Clément Hurel',
-                                //  'type': 'to'
-                                //},
-                                {
-                                  'email': 'alex@therefore.ca',
-                                  'name': 'Alex De Winne',
-                                  'type': 'to'
-                                },
-                                //{
-                                //  'email': 'sean@therefore.ca',
-                                //  'name': 'Sean De Rioux',
-                                //  'type': 'to'
-                                //}
-                                ],
-                                'autotext': 'true',
-                                'subject': 'New Contact Form Submission',
-                                'html': 'Contact information : <br/>' +
-                                'Name : ' + name + '<br/>' +
-                                'Email : ' + email + '<br/>' +
-                                'Comment : ' + comment + '<br/>'
-                              }
-                            }
-                          }).done(function(response) {
-                            $('#contact-calltoaction .inner.container').html('<div class = "call-to-action col-12"><h1>Thank you for contacting us</h1><p class = "lead">Someone will be in touch shortly!</p></div>')
-                            
-                          });
-                        },
-                      }
-                      ); 
+      // Here I do nothing, but you could do something like display 
+      // the error messages to the user, log, etc.
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault();
+      var name = $('#contact-form input#name').val();
+      var email = $('#contact-form input#email').val();
+      var comment = $('#contact-form #comment').val();
+      $.ajax({
+        url:'/contact-process',
+        data: {
+            'name' : name,
+            'email' : email,
+            'comment': comment
+          }
+        }).done(function(response) {
+          $('#contact-calltoaction .inner.container').html('<div class = "call-to-action col-12"><h1>Thank you for contacting us</h1><p class = "lead">Someone will be in touch shortly!</p></div>')
+          
+        });
+      },
+    }
+  ); 
 });
