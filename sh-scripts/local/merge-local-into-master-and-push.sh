dba38b51ee9aba9af5a0d65b3678feeaf4e3441f
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-11-04 11:06:27 -0500

    - adding some helper scripts to allow pulling the repo, installing dependencies, and restart the forever service/site, and local git operations

---
+++ sh-scripts/local/merge-local-into-master-and-push.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git checkout master
+git merge local
+git push
+git checkout local
---
+++ sh-scripts/local/merge-master-into-local.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git checkout master
+git pull
+git checkout local
+git merge master
