e22cf32811134bee15dc372bcb2b6b047edcce2c
Kana Takemura <kana@therefore.ca>
2017-01-23 11:41:20 -0500

    *Revising a form

--- app/scripts/estimate.js
+++ app/scripts/estimate.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
-  $("#estimate-form").submit(function ($form, event) {
+  $("#estimate-form").submit(function (event) {
+    event.preventDefault();
+  });
+  $("input,textarea").jqBootstrapValidation(
+    {
+      preventSubmit: true,
+      submitError: function ($form, event, errors) {
+        // Here I do nothing, but you could do something like display
+        // the error messages to the user, log, etc.
+      },
+      submitSuccess: function ($form, event) {
         event.preventDefault();
-        var name = $('#estimate-form input#name').val();
-        var email = $('#estimate-form input#email').val();
-        var comment = $('#estimate-form #comment').val();
+        var name = $('#estimate input#name').val();
+        var email = $('#estimate input#email').val();
+        var comment = $('#estimate #comment').val();
 
         $.ajax({
           url: '/estimate-process',
@@ -14,8 +24,10 @@ $(document).ready(function () {
           }
         }).done(function (response) {
           if (response && !!response.success) {
-            $('#estimate-form').html('<div class = "call-to-action col-12"><h1>Your message was sent successfully.</h1><p class = "lead">You will hear back from us shortly!</p></div>')
-          } 
+            $('#contact-calltoaction .inner.container').html('<div class = "call-to-action col-12"><h1>Your message was sent successfully.</h1><p class = "lead">You will hear back from us shortly!</p></div>')
+          } else {
+            formError.text("There was an error submitting your request. Please try again.");
+          }
         });
       }
     }
